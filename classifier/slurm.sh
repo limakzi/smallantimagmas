25cccc8c82e98e841da43c458ea0098c49a3a03b
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-06-09 17:39:15 +0200

    feat: Add more slurm options

--- classifier/slurm.sh
+++ classifier/slurm.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#SBATCH -p ampere
+
+
+#SBATCH --partition ampere
 #SBATCH -t 1-00:00:00
 #SBATCH -o ./logs/gap-stdout-%j.txt
 #SBATCH -e ./logs/gap-stderr-%j.txt
+
+#SBATCH --job-name=smallantimagmas-classifier
+#SBATCH --no-requeue
+#SBATCH --verbose
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
 #SBATCH --array=1-16:10
 
 set -x
